f6eb70f34356e0f5cb352791ddd738530551aa68
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-11-20 16:51:14 +0530

    replace favicon with new icon and update homepage layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
-
+import Image from "next/image";
 export default function Home() {
   return (
     <div className="text-5xl font-bold bg-white text-black m-2 p-2">
-      Site is Under Development..................
+      <Image src="/logo.png" width={100} height={100} alt="logo"/>
+      <div>Turant</div>
+      
     </div>
   );
 }
