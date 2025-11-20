cde90fab2835de8a7c2e88f7c09b3c041dff8a2e
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-11-20 16:06:02 +0530

    Initial Commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="text-5xl font-bold bg-white text-black m-2 p-2">
+      Site is Under Development..................
+    </div>
+  );
+}
