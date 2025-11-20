a2b39f876895e8f6e65691439a8c55a9a2d818a4
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2025-11-21 02:03:02 +0530

    enhance navigation and layout: add cursor pointer to menu items, integrate Poppins font, and update homepage content

--- app/Components/Nav1.tsx
+++ app/Components/Nav1.tsx
@@ -3,16 +3,16 @@ import React from 'react'
 
 const Nav1 = () => {
   return (
-    <div className='bg-black/5 flex justify-between p-2'>
-      <div className='text-black text-4xl font-semibold'>TURANT</div>
-      <ul className='flex gap-5 font-bold text-2xl'>
-        <li>Home</li>
-        <li>About Us</li>
-        <li>Service</li>
+    <div className='bg-black/5 flex justify-between py-2 px-4 rounded-full font-[family-name:var(--font-poppins)]'>
+      <div className='text-black text-4xl font-bold my-auto w-1/6 text-center'>TURANT</div>
+      <ul className='flex gap-10 font-bold text-2xl my-auto'>
+        <li className='cursor-pointer'>Home</li>
+        <li className='cursor-pointer'>About Us</li>
+        <li className='cursor-pointer'>Services</li>
       </ul>
       <div className='flex gap-5'>
         <Image src="/abc.png" width={33} height={33} alt='abc' />
-        <button className='text-white bg-[#007D37] font-bold px-8 py-2 m-1 rounded-full text-2xl shadow-[0px_6px_0px_rgba(0,0,0,0.2)] active:shadow-none active:translate-y-[6px] transition-all cursor-pointer'>
+        <button className='text-white bg-[#007D37] font-bold px-8 py-2 m-1 rounded-full text-2xl shadow-[0px_6px_0px_rgba(0,0,0,0.25)] active:shadow-none active:translate-y-[6px] transition-all cursor-pointer hover:bg-[#029240]'>
   Join
 </button>
       </div>
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,31 @@ import Nav1 from "./Components/Nav1";
 
 export default function Home() {
   return (
-    <div className="">
-      <Nav1></Nav1>      
-      
+    <div>
+      <div className="relative w-full h-screen overflow-hidden">
+        <video
+          src="/back.mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute object-contain -z-10"
+        ></video>
+
+        <div className="relative z-10 m-2 top-3 p-2">
+          <Nav1 />
+        </div>
+        <div className="w-full text-center font-[family-name:var(--font-poppins)] font-bold text-5xl my-10 py-10">
+        <div className="my-4 py-2">
+          Khana, Gaadi, ya PG?
+        </div>
+        <div className="my-4 py-2">
+        Bas Turant Karo
+        </div>
+        </div>
+
+      </div>
+
     </div>
   );
 }
