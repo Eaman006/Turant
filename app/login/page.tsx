49373842e8e94f5dd3502d958535a22468ac1eb5
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2025-11-24 22:28:35 +0530

    feat: enhance login page content and structure

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -4,7 +4,22 @@ const page = () => {
   return (
     <div>
       <div>
-        
+        Namaste!  
+      </div>
+      <div>
+        Enter your number to start
+      </div>
+      <div>
+        Phone Number
+      </div>
+      <div>
+        GET OTP
+      </div>
+      <div>
+        OR
+      </div>
+      <div>
+        Login with Google
       </div>
     </div>
   )
