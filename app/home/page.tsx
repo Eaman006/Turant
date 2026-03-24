bc5a6df3284a48fe6f5478d10d716170eefb2a56
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2026-03-24 23:50:11 +0530

    feat: enhance home page layout and content; add new image and description for better user engagement

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -5,15 +5,28 @@ import Sidebar from '../Components/Sidebar'
 const page = () => {
   return (
     <div className=''>
-      <div className='m-0'>
+      <div className=''>
         <Sidebar />
         <div className='ml-28 mr-10 pt-10'>
           <div className='shadow-gray-300 shadow-md flex rounded-xl p-4 gap-5 border-gray-300 border-2'>
             <Image src={"/sh.png"} height={20} width={20} alt='s'></Image>
-            <input className='w-full h-5' type="text" placeholder='search for fruit or cabs' />
+            <input className='w-full h-5 outline-none' type="text" placeholder='search for fruit or cabs' />
+
           </div>
+          <div className='bg-gradient-to-r from-[#E67E22]/50 to-[#F39C12]/50 mt-10 flex justify-center items-center p-4 rounded-2xl'>
+            <div className='font-bold text-5xl text-white m-6 p-6'>
+              No Middlemen. No Commissions. Just Direct Contacts.
+              <div className='font-light text-lg my-2 py-2'>Connect directly with local drivers and shopkeepers. Fair prices for everyone.</div>
+            </div>
+            <div className='m-4 p-4'>
+              <Image className='object-fill' src={"/cc.png"} height={255} width={438} alt='cc'></Image>
+            </div>
+          </div>
+          <div></div>
         </div>
+
       </div>
+
 
     </div>
   )
