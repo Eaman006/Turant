34dd28ebc5e22fa3b5ea0d87f5408ebee63b37b0
MD EAMAN ADEEP <eamanadeep006@gmail.com>
2026-03-24 16:10:47 +0530

    style: adjust input field styling for better appearance in home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -8,9 +8,9 @@ const page = () => {
       <div className='m-0'>
         <Sidebar />
         <div className='ml-28 mr-10 pt-10'>
-          <div className='shadow-gray-300 shadow-md flex p-4 rounded-xl gap-5 border-gray-300 border-2'>
+          <div className='shadow-gray-300 shadow-md flex rounded-xl p-4 gap-5 border-gray-300 border-2'>
             <Image src={"/sh.png"} height={20} width={20} alt='s'></Image>
-            <input type="text" placeholder='search for fruit or cabs' />
+            <input className='w-full h-5' type="text" placeholder='search for fruit or cabs' />
           </div>
         </div>
       </div>
