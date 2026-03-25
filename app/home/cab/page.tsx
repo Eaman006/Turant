b611287595c6cc0faa865941df537d244bc137aa
DakshDugar <Daksh.23bce10667@vitbhopal.ac.in>
2026-03-25 14:18:06 +0530

    feat: implement home cab page layout with search functionality and basic html

--- app/home/cab/page.tsx
+++ app/home/cab/page.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Image from 'next/image'
 
 const page = () => {
   return (
-    <div></div>
+    <div>
+      <div className='shadow-gray-300 shadow-md flex rounded-xl p-4 gap-5 border-gray-300 border-2'>
+                  <Image src={"/sh.png"} height={20} width={20} alt='s'></Image>
+                  <input className='w-full h-5 outline-none' type="text" placeholder='search for fruit or cabs' />
+      
+      </div>
+      <div>
+        HOME &gt; CABS & AUTOS 
+      </div>
+      <div>
+        Cabs & Autos in Kothri Kalan
+      </div>
+      <div>
+        Find reliable transportation options near your hostel. Verified drivers with
+        <div>transparent routes.</div>
+      </div>
+    </div>
   )
 }
 
